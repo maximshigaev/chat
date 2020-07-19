53211ff798fbf34b863dfb4586368c4cd7615f07
Maxim <financial_monster@mail.ru>
2020-07-19 08:15:59 +0400

    Add global styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 
 import { App } from './components';
 
+import './scss/reset.scss';
+import './scss/scaffolding.scss';
+
 ReactDOM.render(
     <App />,
     document.getElementById('root')
