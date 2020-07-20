21429d76aaf21a0166fc93d233154ee1df615807
Maxim <financial_monster@mail.ru>
2020-07-20 08:29:03 +0400

    Add the ChannelsList component and styles for it

---
+++ src/components/ChannelsList/ChannelsList.jsx
@@ -0,0 +1,23 @@
+import React, {useContext} from 'react';
+import {observer} from 'mobx-react';
+
+import {StoreContext} from '../../context';
+import {ChannelsItem} from '../';
+import {MenuTitle} from '../';
+
+import './ChannelsList.scss';
+
+const ChannelsList = observer(() => {
+    const store = useContext(StoreContext);
+
+    return (
+        <nav className="channels">
+            <MenuTitle title="Channels" quantity={store.channels.length} />
+            <ul className="channels__list">
+                {store.channels.map((channel) => <ChannelsItem key={channel.id} channel={channel} />)}
+            </ul>
+        </nav>
+    );
+});
+
+export {ChannelsList};
