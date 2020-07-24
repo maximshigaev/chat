96d74d20949930b74462ba553794bfdeb3600849
Maxim <financial_monster@mail.ru>
2020-07-24 16:07:25 +0400

    Refactoring of the Menu component

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -7,12 +7,12 @@ import './Menu.scss';
 
 const Menu = () => (
     <section className="menu">
-        <h2 className="menu__title">
+        <h2 className="menu-title">
             Nomad List
-            <button className="menu__settings-button" title="Settings" />
-            <button className="menu__dropdown-button" title="Expand" />
+            <button className="menu-settings-button" title="Open settings" />
+            <button className="menu-dropdown-button" title="Expand" />
         </h2>
-        <p className="menu__intro">All treads</p>
+        <p className="menu-intro">All threads</p>
         <ChannelsList />
         <FriendsList />
     </section>
