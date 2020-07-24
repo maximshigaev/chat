1675f1840e0efa46259a7b5cbe5b1d7e030601e8
Maxim <financial_monster@mail.ru>
2020-07-24 10:24:18 +0400

    Add the ChatHeader component and styles for it

---
+++ src/components/ChatHeader/ChatHeader.jsx
@@ -0,0 +1,32 @@
+import React, {useContext} from 'react';
+import {observer} from 'mobx-react';
+import cn from 'classnames';
+
+import {StoreContext} from '../../context';
+import {SearchBar} from '../';
+
+import './ChatHeader.scss';
+
+const ChatHeader = observer(() => {
+    const {currentChannel} = useContext(StoreContext);
+    const {isFavourite, participantsCount, title} = currentChannel;
+    const favouriteBtnClass = cn(`chat-favourite-btn`, {'chat-favourite-btn--in-list': isFavourite});
+
+    return (
+        <div className="chat-header">
+            <h2 className="chat-title">
+                {`# ${title.toLowerCase()}`}
+            </h2>
+            <button className={favouriteBtnClass} title={isFavourite ? `Remove favourite` : `Make favourite`} />
+            <div className="chat-header-wrapper">
+                <span className="chat-participants">
+                    {participantsCount}
+                </span>
+                <SearchBar />
+                <button className="chat-notification-btn" title="See notifications" />
+            </div>
+        </div>
+    );
+});
+
+export {ChatHeader};
