552c8fe114c926ef35575d8c90cf9bb9bf7c501d
Maxim <financial_monster@mail.ru>
2020-07-24 10:22:35 +0400

    Add the Chat component and styles for it

---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,37 @@
+import React, {useContext, useEffect} from 'react';
+import {useLocation} from 'react-router-dom';
+import {observer} from 'mobx-react';
+
+import {ChatHeader} from '../';
+import {StoreContext} from '../../context';
+import {MessageList} from '../';
+import {ChatFooter} from '../';
+
+import './Chat.scss';
+
+const Chat = observer(() => {
+    const currentChannelId = useLocation().pathname.slice(1);
+    const {getCurrentMessages, currentChannel} = useContext(StoreContext);
+    
+    useEffect(() => {
+        getCurrentMessages(currentChannelId);
+    }, [currentChannelId, getCurrentMessages]);
+
+    if(!currentChannel) {
+        return (
+            <section className="chat chat--empty">
+                Please, select a channel
+            </section>
+        );
+    }
+
+    return (
+        <main className="chat">
+            <ChatHeader channel={currentChannel} />
+            <MessageList />
+            <ChatFooter />
+        </main>
+    );
+});
+
+export {Chat};
