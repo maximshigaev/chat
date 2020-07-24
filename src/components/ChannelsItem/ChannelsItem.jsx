1cb1213375e1c4912d3a22b64d2b26a4f5f03386
Maxim <financial_monster@mail.ru>
2020-07-24 16:10:53 +0400

    Refactoring of the ChannelsItem component

--- src/components/ChannelsItem/ChannelsItem.jsx
+++ src/components/ChannelsItem/ChannelsItem.jsx
@@ -1,22 +1,18 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import cn from 'classnames';
-
-import {StoreContext} from '../../context';
+import {NavLink} from 'react-router-dom';
 
 import './ChannelsItem.scss';
 
-const ChannelsItem = ({channel}) => {
-    const store = useContext(StoreContext);
-    const isCurrentChannel = store.currentChannel && store.currentChannel.id === channel.id;
-    const itemClass = cn(`channels-item`, {'channels-item--active': isCurrentChannel});
-
-    return (
-        <li className={itemClass} title={channel.title}>
+const ChannelsItem = ({channel}) =>  (
+    <li className="channels-item">
+        <NavLink className="channels-link" activeClassName="channels-link--active"
+            title={channel.title} to={`/${channel.id}`}
+        >
             {`# ${channel.title.toLowerCase()}`}
-        </li>
-    );
-}
+        </NavLink>
+    </li>
+);
 
 ChannelsItem.propTypes = {
     channel: PropTypes.exact({
