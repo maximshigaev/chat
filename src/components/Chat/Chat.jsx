e455e3b397421837380bd5080ba7cf0013236277
Maxim <financial_monster@mail.ru>
2020-07-31 12:37:47 +0400

    Add tablet styles for the Chat component

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,6 +1,7 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useCallback} from 'react';
 import {useLocation} from 'react-router-dom';
 import {observer} from 'mobx-react';
+import cn from 'classnames';
 
 import {ChatHeader} from '../';
 import {StoreContext} from '../../context';
@@ -11,27 +12,38 @@ import './Chat.scss';
 
 const Chat = observer(() => {
     const currentChannelId = useLocation().pathname.slice(1);
-    const {getCurrentMessages, currentChannel} = useContext(StoreContext);
-    
+    const {getCurrentMessages, currentChannel, setIsMenuOpened, setIsProfileOpened, isMenuOpened,
+        isProfileOpened
+    } = useContext(StoreContext);
+    const mainClass = cn(`chat`, {'chat--empty': !currentChannel});
+
     useEffect(() => {
         if (currentChannelId) {
             getCurrentMessages(currentChannelId);
         }
     }, [currentChannelId, getCurrentMessages]);
 
-    if (!currentChannel) {
-        return (
-            <section className="chat chat--empty">
-                Please, select a channel
-            </section>
-        );
-    }
+    const handleOpenMenuBtnClick = useCallback(() => setIsMenuOpened(true), [setIsMenuOpened]);
+    const handleOpenProfileBtnClick = useCallback(() => setIsProfileOpened(true), [setIsProfileOpened]);
 
     return (
-        <main className="chat">
-            <ChatHeader />
-            <MessageList />
-            <ChatFooter />
+        <main className={mainClass}>
+            {!isMenuOpened &&
+                <button className="chat-open-btn chat-open-btn--menu" title="Open menu" onClick={handleOpenMenuBtnClick} />
+            }
+            {!isProfileOpened &&
+                <button className="chat-open-btn chat-open-btn--profile" title="Open profile"
+                    onClick={handleOpenProfileBtnClick}
+                />
+            }
+            {!currentChannel && `Please, select a channel`}
+            {currentChannel &&
+                <>
+                    <ChatHeader />
+                    <MessageList />
+                    <ChatFooter />
+                </>
+            }
         </main>
     );
 });
