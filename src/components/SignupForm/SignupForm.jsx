0367640328e61b84fbd6f3daf4ac7550655a76d0
Maxim <financial_monster@mail.ru>
2020-08-01 10:22:33 +0400

    Add the checking of repeated registration feature

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useCallback} from 'react';
 import {Formik, Form} from 'formik';
 import {observer} from 'mobx-react';
 
@@ -12,21 +12,42 @@ import './SignupForm.scss';
 import avatarImg from "../../img/no-avatar.png";
 
 const SignupForm = observer(() => {
-    const {createUser} = useContext(StoreContext);
+    const {createUser, users} = useContext(StoreContext);
+    const [wasRegisteredEmailError, setWasRegisteredEmailError] = useState(false);
+    const [wasRegisteredUserNameError, setWasRegisteredUserNameError] = useState(false);
+    const createErrorMessage = useCallback((field) => (
+        <div className="signup-message">
+            {`User with this ${field} is already registered`}
+        </div>
+    ), []);
 
     const handleSignupFormSubmit = (formData) => {
-        createUser({
-            ...formData,
-            avatar: avatarImg,
-            jobTitle: null,
-            timeZone: null,
-            fb: null,
-            tw: null,
-            inst: null,
-            lkdn: null,
-            isProfileOnline: true,
-        });
+        const registeredEmailUser = users.find((user) => user.email === formData.email);
+        const registeredUserNameUser = users.find((user) => user.userName === formData.userName);
+
+        if (registeredEmailUser) {
+            setWasRegisteredEmailError(true);
+        } else if (registeredUserNameUser) {
+            setWasRegisteredUserNameError(true);
+        } else {
+            createUser({
+                ...formData,
+                avatar: avatarImg,
+                jobTitle: null,
+                timeZone: null,
+                fb: null,
+                tw: null,
+                inst: null,
+                lkdn: null,
+                isProfileOnline: true,
+            });
+        }
     };
+
+    const handleFocus = useCallback(() => {
+        setWasRegisteredUserNameError(false);
+        setWasRegisteredEmailError(false);
+    }, []);
 
     return (
         <Formik initialValues={signupFormShape.initialValues} onSubmit={handleSignupFormSubmit}
@@ -34,18 +55,20 @@ const SignupForm = observer(() => {
         >
             {({isValid}) => (
                 <Form className="signup-form">
+                    {wasRegisteredEmailError && createErrorMessage(`email`)}
+                    {wasRegisteredUserNameError && createErrorMessage(`username`)}
                     <FormErrorMessage name="firstName" modifier="firstname" isSignupPage={true} />
                     <FormErrorMessage name="surName" modifier="surname" isSignupPage={true} />
                     <FormErrorMessage name="userName" modifier="username" isSignupPage={true} />
                     <FormErrorMessage name="email" modifier="email" isSignupPage={true} />
                     <FormErrorMessage name="password" modifier="password" isSignupPage={true} />
                     <FormErrorMessage name="skype" modifier="skype" isSignupPage={true} />
-                    <FormField name="firstName" type="text" label="Firstname" isSignupPage={true} />
-                    <FormField name="surName" type="text" label="Surname" isSignupPage={true} />
-                    <FormField name="userName" type="text" label="Username" isSignupPage={true} />
-                    <FormField name="email" type="email" label="Email" isSignupPage={true} />
-                    <FormField name="password" type="password" label="Password" isSignupPage={true} />
-                    <FormField name="skype" type="text" label="Skype" isSignupPage={true} />
+                    <FormField name="firstName" type="text" label="Firstname" isSignupPage={true} onFocus={handleFocus} />
+                    <FormField name="surName" type="text" label="Surname" isSignupPage={true} onFocus={handleFocus} />
+                    <FormField name="userName" type="text" label="Username" isSignupPage={true} onFocus={handleFocus} />
+                    <FormField name="email" type="email" label="Email" isSignupPage={true} onFocus={handleFocus} />
+                    <FormField name="password" type="password" label="Password" isSignupPage={true} onFocus={handleFocus} />
+                    <FormField name="skype" type="text" label="Skype" isSignupPage={true} onFocus={handleFocus} />
                     <button className="signup-btn" title="Sign up" disabled={!isValid} type="submit">
                         Sign up
                     </button>
